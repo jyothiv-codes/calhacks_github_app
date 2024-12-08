df5021857fcb0a085a8c1a7c8a9acad077ea5bdd
Mrunmayeed <mrunmayee.dhapre.98@gmail.com>
2024-12-07 21:14:05 -0800

    login page

--- app.py
+++ app.py
@@ -20,17 +20,24 @@ PASSWORD = "pass123"
 
 # Login Page
 if "user" not in st.session_state:
-    st.title("Login to Chillbert")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    
-    if st.button("Login"):
-        if username == USERNAME and password == PASSWORD:
-            st.session_state.user = username
-            st.success("Login successful!")
-            # st.experimental_rerun()  # Refresh the app to show the main page
-        else:
-            st.error("Invalid username or password.")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown('<div class="col1-background"></div>', unsafe_allow_html=True)
+        st.markdown('<div class="image-container">', unsafe_allow_html=True)
+        st.markdown("![chillbert.gif](https://cdn.dribbble.com/users/7421625/screenshots/18722183/media/57f4bd5aea84c23e226069f65e417704.gif)")
+            
+    with col2:
+        st.title("Login to Chillbert")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        
+        if st.button("Login"):
+            if username == USERNAME and password == PASSWORD:
+                st.session_state.user = username
+                st.success("Login successful!")
+                # st.experimental_rerun()  # Refresh the app to show the main page
+            else:
+                st.error("Invalid username or password.")
 else:
     st.title(f"Hello {st.session_state.user}! Start a Conversation or Analyse Chat")
     if st.button("Logout"):
