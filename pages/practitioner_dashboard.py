e649125e3a98ed77572637889e2596e324e1683a
Mrunmayeed <mrunmayee.dhapre.98@gmail.com>
2024-12-08 22:06:26 -0800

    login changes

--- pages/practitioner_dashboard.py
+++ pages/practitioner_dashboard.py
@@ -2,28 +2,30 @@ import streamlit as st
 from src.analyse_chat import AnalyseChat
 
 
-# if "user" not in st.session_state:
-#     st.error("Please Login to continue")
+if "user" not in st.session_state:
+    st.error("Please Login to continue")
+elif st.session_state['user']!='admin':
+    st.error("You do not have access to this")
 
-# else:
-if 'ac' not in st.session_state and 'patient' in st.session_state:
-    ac = AnalyseChat(st.session_state['patient'])
-    st.session_state['ac'] = ac
+else:
+    if 'ac' not in st.session_state and 'patient' in st.session_state:
+        ac = AnalyseChat(st.session_state['patient'])
+        st.session_state['ac'] = ac
 
-col1, col2 = st.columns(2)
-with col1:
-    st.markdown('<div class="col1-background"></div>', unsafe_allow_html=True)
-    st.markdown('<div class="image-container">', unsafe_allow_html=True)
-    st.markdown("![chillbert.gif](https://cdn.dribbble.com/users/7421625/screenshots/18722183/media/57f4bd5aea84c23e226069f65e417704.gif)")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown('<div class="col1-background"></div>', unsafe_allow_html=True)
+        st.markdown('<div class="image-container">', unsafe_allow_html=True)
+        st.markdown("![chillbert.gif](https://cdn.dribbble.com/users/7421625/screenshots/18722183/media/57f4bd5aea84c23e226069f65e417704.gif)")
 
-with col2:
-    st.markdown('<h3>Ask a Question to Chillbert!</h3>', unsafe_allow_html=True)
-    st.text_input("Enter patient name",key='patient')
-    question = st.text_input("Enter your question here")
-    if st.button("Submit"):
-        ac = st.session_state['ac']
-        response = ac.askChatbot(question)
-        st.write(response)
+    with col2:
+        st.markdown('<h3>Ask a Question to Chillbert!</h3>', unsafe_allow_html=True)
+        st.text_input("Enter patient name",key='patient')
+        question = st.text_input("Enter your question here")
+        if st.button("Submit"):
+            ac = st.session_state['ac']
+            response = ac.askChatbot(question)
+            st.write(response)
             
 st.sidebar.image("ChillbertLogo-removebg-preview.png", use_container_width=True)
 
